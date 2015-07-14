49a19abc5744832f64eff3e7f2101331ece93e90
Brendan Grady <cyhtan@gmail.com>
2015-07-14 10:01:00 -0400

    change filtering to exclusive rather than inclusive

--- js/class-finder/app.js
+++ js/class-finder/app.js
@@ -1,6 +1,5 @@
 /*
     TODO: 
-        exclusive filters
         dynamically generate filters (+ USE LOCAL STORAGE TO CACHE?cache to local storage w/ a time stamp, and if < 1 day elapsed, recreate )
 
         # of results for each option
@@ -8,14 +7,12 @@
         elastic dropdowns
 
         modularize under js/class-finder
-
-
 */
 
 
 var classFinder = angular.module('classFinder', ['ui.bootstrap']);
 
-classFinder.controller('SearchCtrl', ['$scope', '$http', function(scope, http) {
+classFinder.controller('SearchCtrl', ['$scope', '$http', function (scope, http) {
     /*
     AJAX Not Working Logs Error: XMLHttpRequest cannot load https://yogainternational.com/json/classes. No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'null' is therefore not allowed access.
     http.get('https://yogainternational.com/json/classes').success(function(data) { scope.classes = data; });
@@ -24,10 +21,12 @@ classFinder.controller('SearchCtrl', ['$scope', '$http', function(scope, http) {
 
     scope.dropdowns = [
                         {name:'Duration',        menuOptions:['15-min','30-min','45-min','60-min','75-min','90-min']},
-                        {name:'Level',           menuOptions:['Advanced','All-Levels','Beginners','Intermediate','Restorative']},
+                        {name:'Level',           menuOptions:['advanced','all-levels','beginners','intermediate','restorative']},
                         {name:'Instructor',      menuOptions:['Shari Friedrichsen','Luke Ketterhagen']},
                         {name:'Focus',           menuOptions:["deeper-dimensions", "prana-vayus", "calming", "gentle", "breath-and-pranayama", "core", "hip-opening", "twists", "vinyasa", "forward-bends", "strength", "back-support", "healing", "yoga-therapy-yoga-classes", "stress-relief", "energize", "backbends", "inversions", "arm-balances", "prenatal", "bandhas", "pm", "peak-pose", "for-men", "meditation-preparation", "for-women", "5-elements", "chakras-yoga-classes", "alignment", "ayurveda", "am"]}
                       ];
+                      
+                      
 
     scope.activeFilters = {
                             Duration:  [],
@@ -58,50 +57,33 @@ classFinder.controller('SearchCtrl', ['$scope', '$http', function(scope, http) {
         return !scope.searchText || re.test(obj.title) || re.test(obj.author);
     };
 
+    scope.dropdownFilter = function (obj) {
 
-    scope.durationFilter = function (obj) {
-        if ( !scope.activeFilters.Duration.length ) { return true; }
-        for (var i = 0; i < scope.activeFilters.Duration.length; i++) {
-            if ( obj.duration[0] === scope.activeFilters.Duration[i] ) { return true; }    
-        }
-        return false;
-    };
-
-    scope.levelFilter = function (obj) {
-        if ( !scope.activeFilters.Level.length ) { return true; }
-        for (var i = 0; i < scope.activeFilters.Level.length; i++) {
-            if ( obj.level[0] === scope.activeFilters.Level[i].toLowerCase() ) { return true; }    
-        }
-        return false;
-    };
-
-    scope.instructorFilter = function (obj) {
-        if ( !scope.activeFilters.Instructor.length ) { return true; }
-        for (var i = 0; i < scope.activeFilters.Instructor.length; i++) {
-            if ( obj.author === scope.activeFilters.Instructor[i] ) { return true; }    
-        }
-        return false;
-    };
-
-    scope.focusFilter = function (obj) {
-        if ( !scope.activeFilters.Focus.length ) { return true; }
-        for (var i = 0; i < scope.activeFilters.Focus.length; i++) {
-            for (var j = 0; j < obj.focus.length; j++){
-                if ( obj.focus[j] === scope.activeFilters.Focus[i].toLowerCase() ) { return true; }    
+        function hasValues (arrayToCheck, values) {
+            if ( ! Array.isArray(arrayToCheck) || ! Array.isArray(values) ) {
+                throw new Error('Invalid arguments to hasValues function. Both arguments must be arrays.');
             }
+
+            for (var i = 0; i < values.length; i++) {                
+                // If arrayToCheck is missing any of the values provided, return false
+                if ( arrayToCheck.indexOf(values[i]) === -1 ) {
+                    return false;
+                }
+            }
+            // If arrayToCheck is has all items in values, return true (also works if values has no items)
+            return true;
         }
-        return false;
-    };
 
+        if  ( 
+              hasValues(obj.duration, scope.activeFilters.Duration) &&
+              hasValues(obj.level, scope.activeFilters.Level) &&
+              hasValues([obj.author], scope.activeFilters.Instructor) &&
+              hasValues(obj.focus, scope.activeFilters.Focus) 
+            )
+                { return true;
+
+        } else {
+                  return false;
+        }
+    }
 }]);
-
-/*
-classFinder.filter('searchFilter', function() {
-    return function (obj) {
-        var re = new RegExp($scope.searchText, 'i');
-        return !$scope.searchText || re.test(obj.title) || re.test(obj.author);
-    };
-});
-*/
-
-
