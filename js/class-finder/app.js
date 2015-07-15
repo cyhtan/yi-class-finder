836a584733a762ee4795cd920069966a63fd80b4
Brendan Grady <cyhtan@gmail.com>
2015-07-14 22:50:31 -0400

    add display number of matching results on a given filter functionality, and extend elastic filtering to input from search

--- js/class-finder/app.js
+++ js/class-finder/app.js
@@ -1,11 +1,3 @@
-/*
-    TODO: 
-
-        # of results for each option
-
-        modularize under js/class-finder
-*/
-
 var classFinder = angular.module('classFinder', ['ui.bootstrap']);
 
 classFinder.controller('SearchCtrl', ['$scope', '$http', function (scope, http) {
@@ -14,15 +6,15 @@ classFinder.controller('SearchCtrl', ['$scope', '$http', function (scope, http) 
     http.get('https://yogainternational.com/json/classes').success(function(data) { scope.classes = data; });
     */
     scope.classes = classes;
-
+    // TODO: rename this to resultsOfFilter so doesn't begin with verb
     scope.filterResults;
 
     // Initial value prior to dynamic population of menuOptions.
     scope.dropdowns = [
-                        {name:'Duration',   menuOptions:[]},
-                        {name:'Level',      menuOptions:[]},
-                        {name:'Instructor', menuOptions:[]},
-                        {name:'Focus',      menuOptions:[]}
+                        {name:'Duration',   menuOptions:{}},
+                        {name:'Level',      menuOptions:{}},
+                        {name:'Instructor', menuOptions:{}},
+                        {name:'Focus',      menuOptions:{}}
                       ];
 
     scope.activeFilters = {
@@ -33,49 +25,57 @@ classFinder.controller('SearchCtrl', ['$scope', '$http', function (scope, http) 
                       };
 
     scope.populateDropdownMenuOptions = function (listOfClasses) {
-        // console.log('beginning menu population with: ', listOfClasses);
+
         function mapDropdownToClasses(nameInDropdown, nameInClasses, classObj) {
             for (var i = 0; i < scope.dropdowns.length; i++) {
                 if (scope.dropdowns[i].name === nameInDropdown) {
-                    scope.dropdowns[i].menuOptions = scope.dropdowns[i].menuOptions.concat(classObj[nameInClasses]);
+                    // Iterate through classObj[nameInClasses] and create a property for each on menuOptions, initialize it to 0 if undefined, then increment
+                    for (var j = 0; j < classObj[nameInClasses].length; j++) {
+                        if (nameInDropdown === 'Instructor') {
+                            scope.dropdowns[i].menuOptions[ classObj[nameInClasses] ] = ( scope.dropdowns[i].menuOptions[ classObj[nameInClasses] ] || 0 ) + 1;
+                            break;
+                        } else {
+                            scope.dropdowns[i].menuOptions[ classObj[nameInClasses][j] ] = ( scope.dropdowns[i].menuOptions[ classObj[nameInClasses][j] ] || 0 ) + 1;
+                        }
+                    }
                 }
             }
         }
 
         // Empty dropdown menus
         for (var i = 0; i < scope.dropdowns.length; i++) {
-            scope.dropdowns[i].menuOptions = [];
+            scope.dropdowns[i].menuOptions = {};
         }
 
-        // Get all possible values 
-        for (var i = 0; i < listOfClasses.length; i++) {
-            mapDropdownToClasses ('Duration',   'duration', listOfClasses[i]);
-            mapDropdownToClasses ('Level',      'level',    listOfClasses[i]);
-            mapDropdownToClasses ('Instructor', 'author',   listOfClasses[i]);
-            mapDropdownToClasses ('Focus',      'focus',    listOfClasses[i]);
+        // Iterate over all classes, and get all possible values
+        for (i = 0; i < listOfClasses.length; i++) {
+            mapDropdownToClasses('Duration',   'duration', listOfClasses[i]);
+            mapDropdownToClasses('Level',      'level',    listOfClasses[i]);
+            mapDropdownToClasses('Instructor', 'author',   listOfClasses[i]);
+            mapDropdownToClasses('Focus',      'focus',    listOfClasses[i]);
         }
-        // Remove duplicates
-        for (var j = 0; j < scope.dropdowns.length; j++) {
-            scope.dropdowns[j].menuOptions = _.uniq(scope.dropdowns[j].menuOptions);
-        }
-    }
+
+        console.log(scope.dropdowns);
+
+    };
+
 
     // TODO: refactor this so that it fires on some reliable event, not just 100ms.
     scope.repopulateDropdowns = function () {
         setTimeout( function() {
             scope.populateDropdownMenuOptions(scope.filterResults);
         }, 100); 
-    }
+    };
 
     scope.addFilter = function (filter, category) {
         // Don't add the filter if it already exists
         if ( scope.activeFilters[category].indexOf(filter) === -1 ) {
             scope.activeFilters[category].push(filter);
         }
-        scope.repopulateDropdowns();
+        scope.repopulateDropdowns();  // TODO: have this fire on a $watch event of activeFilters ?
     };
     scope.removeFilter = function (index, category) {
-        scope.activeFilters[category].splice(index,1);
+        scope.activeFilters[category].splice(index, 1);
         scope.repopulateDropdowns();
     };
     scope.resetFilters = function () {
@@ -83,8 +83,14 @@ classFinder.controller('SearchCtrl', ['$scope', '$http', function (scope, http) 
         for (var prop in scope.activeFilters) {
             scope.activeFilters[prop] = [];
         }
+        // Delete text from search field
+        scope.searchText = '';
         scope.repopulateDropdowns();
-    }
+    };
+
+    scope.$watch('searchText', function(newValue, oldValue) {
+        scope.repopulateDropdowns();
+    });
 
     // Custom filter, checking only .title & .author properties.
     scope.filterSearch = function (obj) {
@@ -92,9 +98,8 @@ classFinder.controller('SearchCtrl', ['$scope', '$http', function (scope, http) 
         return !scope.searchText || re.test(obj.title) || re.test(obj.author);
     };
 
-    scope.filterDropdown = function (obj) {
-
-        function hasValues (arrayToCheck, values) {
+    // Helper function
+    scope.hasValues = function (arrayToCheck, values) {
             if ( ! Array.isArray(arrayToCheck) || ! Array.isArray(values) ) {
                 throw new Error('Invalid arguments to hasValues function. Both arguments must be arrays.');
             }
@@ -107,23 +112,24 @@ classFinder.controller('SearchCtrl', ['$scope', '$http', function (scope, http) 
             }
             // If arrayToCheck is has all items in values, return true (also works if values has no items)
             return true;
-        }
+    };
 
+    scope.filterDropdown = function (obj) {
         if  ( 
-              hasValues(obj.duration, scope.activeFilters.Duration) &&
-              hasValues(obj.level, scope.activeFilters.Level) &&
-              hasValues([obj.author], scope.activeFilters.Instructor) && // note obj.author is not an array, and so must be cast into one
-              hasValues(obj.focus, scope.activeFilters.Focus) 
+              scope.hasValues(obj.duration, scope.activeFilters.Duration) &&
+              scope.hasValues(obj.level, scope.activeFilters.Level) &&
+              scope.hasValues([obj.author], scope.activeFilters.Instructor) && // note obj.author is not an array, and so must be cast into one
+              scope.hasValues(obj.focus, scope.activeFilters.Focus) 
             )
                 { return true;
 
         } else {
                   return false;
         }
-    }
+    };
 
     scope.init = function () {
         scope.populateDropdownMenuOptions(scope.classes);
-    }
+    };
     scope.init();
 }]);
